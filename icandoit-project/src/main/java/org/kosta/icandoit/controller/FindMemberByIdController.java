dcad097b7b77b75de1cb4116e83e3a7cac8d6540
choi9818 <chwechoi9818@naver.com>
2023-06-22 17:44:26 +0900

    Merge branch 'develop' into feature/RegisterMember

--- icandoit-project/src/main/java/org/kosta/icandoit/controller/FindMemberByIdController.java
+++ icandoit-project/src/main/java/org/kosta/icandoit/controller/FindMemberByIdController.java
@@ -1,14 +1,20 @@
-package org.kosta.icandoit.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class FindMemberByIdController implements Controller {
-
-	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
+//package org.kosta.icandoit.controller;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.kosta.icandoit.model.MemberDAO;
+//import org.kosta.icandoit.model.MemberVO;
+//
+//public class FindMemberByIdController implements Controller {
+//
+//	@Override
+//	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		String id = request.getParameter("id");
+//		MemberVO memberVO = MemberDAO.getInstance().findMemberById(id);
+//		if (memberVO == null)
+//			// TODO Auto-generated method stub
+//			return null;
+//	}
+//
+//}
