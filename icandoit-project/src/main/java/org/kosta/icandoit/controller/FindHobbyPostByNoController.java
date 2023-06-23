f040559a19297453b661dfed40af6504d76f23fb
hosose <hosose22@gmail.com>
2023-06-23 12:33:01 +0900

    dosc - UPDATE : FindHobbyPostByNoController

--- icandoit-project/src/main/java/org/kosta/icandoit/controller/FindHobbyPostByNoController.java
+++ icandoit-project/src/main/java/org/kosta/icandoit/controller/FindHobbyPostByNoController.java
@@ -2,6 +2,7 @@ package org.kosta.icandoit.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.kosta.icandoit.model.PostDAO;
 import org.kosta.icandoit.model.PostVO;
@@ -9,11 +10,11 @@ import org.kosta.icandoit.model.PostVO;
 public class FindHobbyPostByNoController implements Controller {
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//		HttpSession session = request.getSession(false);
-//		if (session == null || session.getAttribute("memberVO") == null) {
-//			System.out.println("비인증");
-//			return "redirect:FindPostList.do";
-//		}
+		HttpSession session = request.getSession(false);
+		if (session == null || session.getAttribute("memberVO") == null) {
+			System.out.println("비인증");
+			return "redirect:FindPostList.do";
+		}
 
 		long no = Long.parseLong(request.getParameter("no"));
 		PostVO post = PostDAO.getInstance().findPostDetail(no);
