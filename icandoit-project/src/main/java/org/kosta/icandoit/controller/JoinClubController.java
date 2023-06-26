33a3476e8c6ed5cb0fefd95e0070b059e9852a7a
hosose <hosose22@gmail.com>
2023-06-26 10:04:06 +0900

    docs - JoinClubController, RegisterHobbyPostController

--- icandoit-project/src/main/java/org/kosta/icandoit/controller/JoinClubController.java
+++ icandoit-project/src/main/java/org/kosta/icandoit/controller/JoinClubController.java
@@ -1,7 +1,5 @@
 package org.kosta.icandoit.controller;
 
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,15 +13,15 @@ public class JoinClubController implements Controller {
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		if(request.getMethod().equals("POST")==false)
-			throw new ServletException("POST 방식만 서비스 됩니다");	
-		
+		if (request.getMethod().equals("POST") == false)
+			throw new ServletException("POST 방식만 서비스 됩니다");
+
 		HttpSession session = request.getSession(false);
 		if (session == null || session.getAttribute("memberVO") == null) {
 			System.out.println("비인증");
 			return "redirect:LoginForm.do";
 		}
-		
+
 		MemberVO memberVO = (MemberVO) session.getAttribute("memberVO");
 		String id = memberVO.getId();
 		long postNo = Long.parseLong(request.getParameter("postNo"));
--- icandoit-project/src/main/java/org/kosta/icandoit/controller/RegisterHobbyPostController.java
+++ icandoit-project/src/main/java/org/kosta/icandoit/controller/RegisterHobbyPostController.java
@@ -1,6 +1,5 @@
 package org.kosta.icandoit.controller;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,25 +14,24 @@ public class RegisterHobbyPostController implements Controller {
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 //		if(request.getMethod().equals("POST")==false)
 //			throw new ServletException("POST 방식만 서비스 됩니다");
-		
-		HttpSession session=request.getSession(false);
+
+		HttpSession session = request.getSession(false);
 //		if(session==null||session.getAttribute("memberVO")==null) {
 //			System.out.println("**비인증 상태이므로 게시글 전송 할 수 없습니다**");
 //			return "redirect:FindPostByNo.do";
 //		}	
 //		long postNo;
-		String title=request.getParameter("title");
-		String postContent=request.getParameter("postContent");
-		String img=request.getParameter("img");
+		String title = request.getParameter("title");
+		String postContent = request.getParameter("postContent");
+		String img = request.getParameter("img");
 //		String gatheringType=request.getParameter("gatheringType");
-		String gatheringPeriod=request.getParameter("gatheringPeriod");
-		String categoryType=request.getParameter("categoryType");
+		String gatheringPeriod = request.getParameter("gatheringPeriod");
+		String categoryType = request.getParameter("categoryType");
 //		String timePosted=request.getParameter("timePosted");
-		String currentCount=request.getParameter("currentCount");
-		String maxCount=request.getParameter("maxCount");
-		MemberVO memberVO=(MemberVO) session.getAttribute("memberVO");
-		
-		PostVO post=new PostVO();
+		String maxCount = request.getParameter("maxCount");
+		MemberVO memberVO = (MemberVO) session.getAttribute("memberVO");
+
+		PostVO post = new PostVO();
 		post.setTitle(title);
 		post.setPostContent(postContent);
 		post.setImg(img);
@@ -45,7 +43,7 @@ public class RegisterHobbyPostController implements Controller {
 		post.setMaxCount(Integer.parseInt(maxCount));
 		post.setMemberVO(memberVO);
 		PostDAO.getInstance().posting(post);
-		
+
 		return "FindHobbyPostList.do";
 	}
 
