456fed1bf8b768e270228da35a66e8f433575f3e
hosose <hosose22@gmail.com>
2023-06-28 10:09:54 +0900

    feat - find my hobby post pagination

--- icandoit-project/src/main/java/org/kosta/icandoit/controller/FindMyHobbyPostListController.java
+++ icandoit-project/src/main/java/org/kosta/icandoit/controller/FindMyHobbyPostListController.java
@@ -1,14 +1,12 @@
 package org.kosta.icandoit.controller;
 
-import java.util.ArrayList;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.kosta.icandoit.model.MemberVO;
+import org.kosta.icandoit.model.MyPagePagination;
 import org.kosta.icandoit.model.PostDAO;
-import org.kosta.icandoit.model.PostVO;
 
 public class FindMyHobbyPostListController implements Controller {
 
@@ -21,8 +19,17 @@ public class FindMyHobbyPostListController implements Controller {
 		}
 		MemberVO memberVO = (MemberVO) session.getAttribute("memberVO");
 		String id = memberVO.getId();
-		ArrayList<PostVO> list = PostDAO.getInstance().findMyHobbyPostList(id);
-		request.setAttribute("post", list);
+		String pageNo = request.getParameter("pageNo");
+		MyPagePagination pagination = null;
+		long totalMyClubCount = PostDAO.getInstance().findTotalMyClubCount(id);
+		if (pageNo == null) {
+			pagination = new MyPagePagination(totalMyClubCount);
+		} else {
+			pagination = new MyPagePagination(totalMyClubCount, Long.parseLong(pageNo));
+		}
+		request.setAttribute("pagination", pagination);
+		request.setAttribute("post", PostDAO.getInstance().findMyHobbyPostList(id, pagination));
+
 		request.setAttribute("url", "member-joining-club.jsp");
 		return "layout.jsp";
 	}
